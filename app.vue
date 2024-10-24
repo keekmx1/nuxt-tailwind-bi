47e3240bbc3eef67ddacfdb571316b5a91d92428
keekmx1 <kitthisack180897@gmail.com>
2024-10-24 10:29:28 +0700

    Install daisyUI

--- app.vue
+++ app.vue
@@ -1,16 +1,5 @@
 <template>
-  <div class="card bg-base-100 w-96 shadow-xl">
-  <figure>
-    <img
-      src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-      alt="Shoes" />
-  </figure>
-  <div class="card-body">
-    <h2 class="card-title">Shoes!</h2>
-    <p>If a dog chews shoes whose shoes does he choose?</p>
-    <div class="card-actions justify-end">
-      <button class="btn btn-primary">Buy Now</button>
-    </div>
+  <div>
+    <span class="loading loading-infinity loading-lg"></span>
   </div>
-</div>
 </template>
